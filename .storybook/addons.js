2e412708c08ed790265f64ee3697b44674e768b5
RunningCoderLee <sprint_l@aliyun.com>
2019-12-02 22:25:31 +0800

    chore: add accessibility addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -5,3 +5,4 @@ import "@storybook/addon-storysource/register";
 import "@storybook/addon-docs/register";
 import "@storybook/addon-viewport/register";
 import "@storybook/addon-backgrounds/register";
+import "@storybook/addon-a11y/register";
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,8 @@
-import { configure, addParameters } from "@storybook/react";
+import { configure, addParameters, addDecorator } from "@storybook/react";
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { withA11y } from "@storybook/addon-a11y";
+
+addDecorator(withA11y);
 
 addParameters({
   viewport: {
@@ -9,7 +12,12 @@ addParameters({
   backgrounds: [
     { name: "twitter", value: "#00aced", default: true },
     { name: "facebook", value: "#3b5998" }
-  ]
+  ],
+  a11y: {
+    element: "#root", // optional selector which element to inspect
+    config: {}, // axe-core configurationOptions (https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#parameters-1)
+    options: {} // axe-core optionsParameter (https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#options-parameter)
+  }
 });
 
 // automatically import all files ending in *.stories.js
